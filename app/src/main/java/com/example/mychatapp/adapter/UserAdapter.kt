4d737bf9508b21a90bd4819ff2ff25c07686655d
Chemorei <chemoreidaniel@gmail.com>
2023-05-08 11:23:40 +0300

    first commit

---
+++ app/src/main/java/com/example/mychatapp/adapter/UserAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.mychatapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mychatapp.R
+import com.example.mychatapp.databinding.ItemProfileBinding
+import com.example.mychatapp.model.User
+
+class UserAdapter(var context:Context,var userList:ArrayList<User>):
+RecyclerView.Adapter<UserAdapter.UserViewHolder>()
+{
+    inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val binding :ItemProfileBinding = ItemProfileBinding.bind(itemView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        var  v = LayoutInflater.from(context).inflate(R.layout.item_profile,parent,false)
+            return UserViewHolder(v)
+    }
+
+    override fun getItemCount(): Int = userList.size
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+      val user = userList[position]
+        holder.binding.username.text = user.name
+        Glide.with(context).load(user.profileImage)
+            .placeholder(R.drawable.profile)
+            .into(holder.binding.profile)
+    }
+}
